5b1799592f5725127ddfe9414b9031058160c28b
Daniyar2526 <69908916+Daniyar2526@users.noreply.github.com>
2020-08-19 10:11:15 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************************************************************
+* Hello
+************************************************************************** 
+* Saifuding Daniyaer
+* 08/19/2020
+* Hello
+*************************************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	 System.out.println("<Your Name>");
+	 System.out.println("<Java Project Name>");
+	 System.out.println("<Version Date>");
+	 System.out.println("<Course Number and Section>");
+	 System.out.println();
+	}
+}
